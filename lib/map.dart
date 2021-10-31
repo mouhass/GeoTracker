a3eb2b584e9044ad96caea7335e286d801e2b484
yessinkcharem <64277049+yessinkcharem@users.noreply.github.com>
2021-10-31 10:17:08 +0100

    errors fixed + tracking position

--- lib/map.dart
+++ lib/map.dart
@@ -1,6 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_osm_plugin/flutter_osm_plugin.dart' ;
+import 'package:flutter_osm_plugin/flutter_osm_plugin.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Map extends StatefulWidget {
@@ -9,69 +11,75 @@ class Map extends StatefulWidget {
 }
 
 class _MapState extends State<Map> {
-
   MapController mapController = MapController(
-    initMapWithUserPosition: true,
-    initPosition: null,
+    initMapWithUserPosition: false,
+    initPosition: GeoPoint(latitude: 47.4358055, longitude: 8.4737324),
   );
 
   @override
-   Widget build(BuildContext context) {
-
-    return
-      Scaffold(
+  Widget build(BuildContext context) {
+    return Scaffold(
         appBar: AppBar(
-          title:
-            Text( 'Géo',
-                style: GoogleFonts.portLligatSans(
-                textStyle: Theme.of(context).textTheme.headline1,
-                fontSize: 30,
-                fontWeight: FontWeight.w700,
-                color: Colors.white,
-                ),
-            ),)
-        ,body:
-          OSMFlutter(
-            controller:mapController,
-            trackMyPosition: true,
-            initZoom: 12,
-            minZoomLevel: 8,
-            maxZoomLevel: 14,
-            stepZoom: 1.0,
-            userLocationMarker: UserLocationMaker(
-              personMarker: MarkerIcon(
-                    icon: Icon(
-                      Icons.location_history_rounded,
-                      color: Colors.red,
-                      size: 48,
-                    ),
-                      ),
-              directionArrowMarker: MarkerIcon(
-                  icon: Icon(
-                      Icons.double_arrow,
-                      size: 48,
-                      ),
-                      ),
+          title: Text(
+            'Géo',
+            style: GoogleFonts.portLligatSans(
+              textStyle: Theme.of(context).textTheme.headline1,
+              fontSize: 30,
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
             ),
-            road: Road(
-                startIcon: MarkerIcon(
-                    icon: Icon(
-                    Icons.person,
-                    size: 64,
-                    color: Colors.brown,
-                    ),
-                ),
-                roadColor: Colors.yellowAccent,
-                  ),
-        markerOption: MarkerOption(
-            defaultMarker: MarkerIcon(
-              icon: Icon(
-                Icons.person_pin_circle,
-                color: Colors.blue,
-                size: 56,
-              ),
-            )
+          ),
         ),
-      ));
+        body: OSMFlutter(
+          controller: mapController,
+          trackMyPosition: true,
+          initZoom: 18,
+          minZoomLevel: 2,
+          maxZoomLevel: 19,
+          stepZoom: 1.0,
+          userLocationMarker: UserLocationMaker(
+            personMarker: MarkerIcon(
+              icon: Icon(
+                Icons.location_pin,
+                color: Colors.red,
+                size: 48,
+              ),
+            ),
+            directionArrowMarker: MarkerIcon(
+              icon: Icon(
+                Icons.double_arrow,
+                size: 48,
+              ),
+            ),
+          ),
+          road: Road(
+            startIcon: MarkerIcon(
+              icon: Icon(
+                Icons.person,
+                size: 64,
+                color: Colors.brown,
+              ),
+            ),
+            roadColor: Colors.yellowAccent,
+          ),
+          markerOption: MarkerOption(
+              defaultMarker: MarkerIcon(
+            icon: Icon(
+              Icons.person_pin_circle,
+              color: Colors.blue,
+              size: 56,
+            ),
+          )),
+        ),
+        floatingActionButton: FloatingActionButton(
+            child: Text('P'),
+            onPressed: () async {
+              print('position button pressed');
+              mapController.currentLocation().then( (value) async => await mapController.enableTracking());
+              print('current position');
+            }
+            // mapController.disabledTracking();
+            //mapController.setZoom(zoomLevel: 18);
+            ));
   }
 }
--- lib/welcome.dart
+++ lib/welcome.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:untitled/map.dart';
+import './map.dart';
 
 class WelcomePage extends StatefulWidget {
   WelcomePage({Key? key, this.title}) : super(key: key);
